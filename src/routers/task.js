8a33d92675f1a29018ded49b71335d255e1a16ee
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-07-14 00:33:27 +0530

    Added routers directory to simplify index file and have all endpoints sorted for ease of reading

---
+++ src/routers/task.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const Task = require('../models/task')
+const router = new express.Router()
+
+// POST request to add new task
+router.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+// GET request to get all tasks from db
+router.get('/tasks', async (req, res) => {
+    // Async await implementation
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+
+    // Old Code
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((error) => {
+    //     res.status(500).send(error)
+    // })
+})
+
+// GET request to get single task from db
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    // Async await implementation
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+
+    // Old Code
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((error) => {
+    //     res.status(500).send(error)
+    // })
+})
+
+// PATCH request to update task based on id
+router.patch('/tasks/:id', async (req, res) => {
+    // Checking for valid updates
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid Updates'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidator: true})
+        if (!task) {
+            res.status(404).send()
+        }
+        res.send(task)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// DELETE request to delete task by id from db
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            res.status(404).send()
+        }
+        res.send(task)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
---
+++ src/routers/user.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const User = require('../models/user')
+const router = new express.Router()
+
+// POST request to add new user
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    // Async await implementation
+    try {
+        await user.save()
+        res.status(201).send(user)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+
+    // Old Code
+    // user.save().then(() => {
+    //     res.status(201).send(user)
+    // }).catch((error) => {
+    //     res.status(400).send(error)
+    // })
+})
+
+// GET request to get all users from db
+router.get('/users', async (req, res) => {
+    // Async await implementation
+    try {
+        const users = await User.find({})
+        res.send(users)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+    
+    // Old Code
+    // User.find({}).then((users) => {
+    //     res.send(users)
+    // }).catch((error) => {
+    //     res.status(500).send(error)
+    // })
+})
+
+// GET request to get single user from db
+router.get('/users/:id', async (req, res) => {
+    // Async await implementation
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+
+    // Old Code
+    // User.findById(_id).then((user) => {
+    //     if (!user) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((error) => {
+    //     res.status(500).send(error)
+    // })
+})
+
+// PATCH request to update user based on id
+router.patch('/users/:id', async (req, res) => {
+    // Checking for valid updates
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'age', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid Updates'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// DELETE request to delete user by id from db
+router.delete('/users/:id', async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
