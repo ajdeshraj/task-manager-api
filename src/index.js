1b12323e24293940aa8e49c18dc92434a0d35b1e
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-07-13 15:52:27 +0530

    Added different requests to get user by id

--- src/index.js
+++ src/index.js
@@ -6,19 +6,64 @@ const Task = require('./models/task')
 
 app.use(express.json())
 
+// POST request to add new user
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
     })
 })
 
+// GET request to get all users from db
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+// GET request to get single user from db
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+// GET request to get all tasks from db
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send(error)
+    })
+})
+
+// POST request to add new task
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
     })
