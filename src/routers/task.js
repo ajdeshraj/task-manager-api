2cd849c01cfb117c6cbfd33f6a1b109c4be06b0a
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-07-14 22:44:42 +0530

    Changing patch route to accomodate middleware for both user and task routes

--- src/routers/task.js
+++ src/routers/task.js
@@ -69,7 +69,14 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidator: true})
+        // Using findByIdAndUpdate(Mongoose) doesn't allow Middleware
+        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidator: true})
+
+        const task = await Task.findById(req.params.id)
+        updates.forEach((update) => task[update] = req.body[update])
+        
+        await task.save()
+
         if (!task) {
             res.status(404).send()
         }
