5acc305fc30d9118554f44b3b4770cfd394423c2
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-07-17 18:41:38 +0530

    Messing around with jwt

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     }
+//     else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is down. Check back soon!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -11,3 +24,15 @@ app.use(taskRouter)
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token = jwt.sign({_id: 'abc1234'}, 'thisisrandom', {expiresIn: '7 days'})
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisisrandom')
+    console.log(data)
+}
+
+myFunction()
