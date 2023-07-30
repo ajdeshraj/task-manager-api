b0445f72ccb967d2225e54d0adf00331fa6da5be
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-07-30 14:21:14 +0530

    Added auth to relevant task routes and removed unnecessary routes

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const Task = require('../models/task')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 
 // POST request to add new task
-router.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((error) => {
-        res.status(400).send(error)
+router.post('/tasks', auth, async (req, res) => {
+    // const task = new Task(req.body)
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
     })
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch(error) {
+        res.status(400).send(error)
+    }
 })
 
 // GET request to get all tasks from db
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     // Async await implementation
     try {
-        const tasks = await Task.find({})
+        const tasks = await Task.find({owner: req.user._id})
         res.send(tasks)
     }
     catch (error) {
@@ -32,11 +39,14 @@ router.get('/tasks', async (req, res) => {
 })
 
 // GET request to get single task from db
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
+    // console.log(_id)
     // Async await implementation
     try {
-        const task = await Task.findById(_id)
+        // const task = await Task.findById(_id)
+        const task = await Task.findOne({_id, owner: req.user._id})
+        // console.log(task)
         if (!task) {
             return res.status(404).send()
         }
@@ -58,7 +68,7 @@ router.get('/tasks/:id', async (req, res) => {
 })
 
 // PATCH request to update task based on id
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     // Checking for valid updates
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
@@ -72,14 +82,15 @@ router.patch('/tasks/:id', async (req, res) => {
         // Using findByIdAndUpdate(Mongoose) doesn't allow Middleware
         // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidator: true})
 
-        const task = await Task.findById(req.params.id)
-        updates.forEach((update) => task[update] = req.body[update])
-        
-        await task.save()
+        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
+        // const task = await Task.findById(req.params.id)
 
         if (!task) {
             res.status(404).send()
         }
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
         res.send(task)
     }
     catch (error) {
@@ -88,9 +99,10 @@ router.patch('/tasks/:id', async (req, res) => {
 })
 
 // DELETE request to delete task by id from db
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        // const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user._id})
         if (!task) {
             res.status(404).send()
         }
