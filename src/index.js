8c858bef6ab5c2049204f4b148062d8966797a85
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-07-31 21:54:59 +0530

    Adding routes to add, delete and retrieve images as user avatar

--- src/index.js
+++ src/index.js
@@ -5,17 +5,29 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, callback) {
+        if (!file.originalname.match(/\.(png)$/)) {
+            return callback(new Error('Only Images are accepted'))
+        }
+
+        callback(undefined, true)
+    }
 })
 
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const router = new express.Router()
 const auth = require('../middleware/auth')
-
-const upload = multer({
-    dest: 'avatars'
-})
 
 // POST request to add new user
 router.post('/users', async (req, res) => {
@@ -104,9 +101,52 @@ router.delete('/users/me', auth, async(req, res) => {
     }
 })
 
+// Calling multer
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, callback) {
+        if (!file.originalname.match(/\.(jgp|jpeg|png)$/)) {
+            return callback(new Error('Only JPG, JPEG and PNG files are accepted'))
+        }
+
+        callback(undefined, true)
+    }
+})
+
 // POST request to upload profile picture for user
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    // Normalising file (image) uploaded
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+// DELETE request to delete profile picture for user
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error() 
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    }
+    catch (error) {
+        res.status(404).send(error)
+    }
 })
 
 module.exports = router
